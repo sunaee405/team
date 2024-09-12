88d64eb7350ffbc541ebd60db822c48ceb3aee1e
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-09-12 17:04:35 +0900

    주석 추가

--- src/main/java/com/example/team/controller/pageConfig.java
+++ src/main/java/com/example/team/controller/pageConfig.java
@@ -4,16 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class pageConfig implements WebMvcConfigurer {
-	
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // static 파일
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
-        // HTML 파일 경로 설정
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/templates/**/*");
-    }
-}
+//@Configuration
+//public class pageConfig implements WebMvcConfigurer {
+//	
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        // static 파일
+//        registry.addResourceHandler("/static/**")
+//                .addResourceLocations("classpath:/static/");
+//        // HTML 파일 경로 설정
+//        registry.addResourceHandler("/**")
+//                .addResourceLocations("classpath:/templates/**/*");
+//    }
+//}
