944b6b86c7900e276647e2a4dce2f6d90c307edb
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-09-12 16:47:25 +0900

    주석삭제

--- src/main/java/com/example/team/TeamApplication.java
+++ src/main/java/com/example/team/TeamApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TeamApplication {
 
 	public static void main(String[] args) {
-		//ㄷㄳ니ㅏ허ㅣㅓ힎
 		SpringApplication.run(TeamApplication.class, args);
 	}
 
