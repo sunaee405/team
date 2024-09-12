62478ee3bc37a2394b61e87fa3c27f4f899b434c
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-09-12 16:05:30 +0900

    test

---
+++ src/main/java/com/example/team/Mapper/testMapper.java
@@ -0,0 +1,13 @@
+package com.example.team.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface testMapper {
+	List<Map<String, Object>> find();
+}
