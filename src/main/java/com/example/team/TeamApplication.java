7211a255209f79069231cbe580e35ae8c3fb38cf
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-09-12 16:58:23 +0900

    충돌 test2

--- src/main/java/com/example/team/TeamApplication.java
+++ src/main/java/com/example/team/TeamApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TeamApplication {
-
+	
+	//충돌 test
 	public static void main(String[] args) {
 		SpringApplication.run(TeamApplication.class, args);
 	}
