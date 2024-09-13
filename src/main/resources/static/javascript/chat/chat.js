b8aae0e3e7eea6046061ed8c1334a8d87db6b784
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-09-13 17:55:01 +0900

    0913

--- src/main/resources/static/javascript/chat/chat.js
+++ src/main/resources/static/javascript/chat/chat.js
@@ -1,6 +1,36 @@
 $(document).on('click', '.chatBtn', function() {
-	debugger;
-	window.open("/myPage/chatRoom", 'myWindow', 'width=800,height=600, resizable=no');
+	
+	
+	let userWidth = window.screen.availWidth;
+	let userHeight = window.screen.availHeight;
+	
+	let width = 500;
+	let height = 700;
+	
+	
+	if(userWidth < width) width = userWidth;
+	if(userHeight < height) height = userHeight;
+	
+	
+	let left = (userWidth - width) / 2;
+	let top = (userHeight - height) / 2;
+	
+	let chatRoom = window.open("/myPage/chatRoom",
+					 'myWindow',
+					 `width=${width},height=${height},
+					  resizable=no,
+					  top=${top},
+					  left=${left}`
+				   );
+	chatRoom.onresize = () => {
+	    // 특정 최대 크기로 제한
+	    let maxWidth = chatRoom.outerWidth;
+	    let maxHeight = hatRoom.outerHeight;
+	    
+	    chatRoom.resizeTo(maxWidth, maxHeight);
+    }
+	 
+//	debugger;				 
 });
 	
 	
