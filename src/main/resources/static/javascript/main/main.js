ce2260b069444176468915a01914d5db6a2fcf84
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-10-17 12:12:38 +0900

    이미지 수정

--- src/main/resources/static/javascript/main/main.js
+++ src/main/resources/static/javascript/main/main.js
@@ -251,6 +251,8 @@ function forProductList(key, typeValue, data) {
 		}
 	}
 	
+	debugger;
+	
 	let productList =
 		`<a href="/product/contentProduct?proNo=${data.PRO_NO}" class="productLink relative group box-border overflow-hidden flex rounded-md cursor-pointer pe-0 pb-2 lg:pb-3 flex-col items-start transition duration-200 ease-in-out transform bg-white ga4_main_latest_product">
 			<div class="relative w-full rounded-md overflow-hidden dim pt-[100%] mb-3 md:mb-3.5">
